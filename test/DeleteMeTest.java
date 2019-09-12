08faf49081e334d49be45fa04d8f3d6f1e441d4b
Ryan Nutt <ryancnutt@katyisd.org>
2019-09-12 06:50:55 -0500

    add helper methods to default test class;

--- test/DeleteMeTest.java
+++ test/DeleteMeTest.java
@@ -1,16 +1,86 @@
-
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.io.*;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
+@SuppressWarnings( "unchecked" )
 public class DeleteMeTest {
-    
-    @Test(timeout=250)
-    public void test() {
-        assertEquals("Testing my sanity", 1, 1); 
+
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+
+    @Test( timeout = 250 )
+    public void test() throws Exception {
+        
+
     }
+
+    private static List<String[]> toList(String words) {
+        List<String[]> list = new ArrayList<>(); 
+        String[] ray = words.split(",");
+        for (String s: ray) {
+            list.add(new String[]{s}); 
+        }
+        return list; 
+    }
+
+    /**
+     * Turns on stdOut output capture
+     */
+    private void captureOut() {
+        System.setOut( new PrintStream( outContent ) );
+    }
+
+    /**
+     * Turns on stdErr output capture
+     */
+    private void captureErr() {
+        System.setErr( new PrintStream( errContent ) );
+    }
+
+    /**
+     * Turns off stdOut capture and returns the contents
+     * that have been captured
+     *
+     * @return
+     */
+    private String getOut() {
+        System.setOut( new PrintStream( new FileOutputStream( FileDescriptor.out ) ) );
+        return outContent.toString().replaceAll( "\r", "" );
+
+    }
+
+    /**
+     * Turns off stdErr capture and returns the contents
+     * that have been captured
+     *
+     * @return
+     */
+    private String getErr() {
+        System.setErr( new PrintStream( new FileOutputStream( FileDescriptor.out ) ) );
+        return errContent.toString().replaceAll( "\r", "" );
+    }
+
+    private <T> T getField( Object instance, String fieldName ) throws Exception {
+        Field fld = instance.getClass().getDeclaredField( fieldName );
+        fld.setAccessible( true );
+        return (T) fld.get( instance );
+    }
+
+    private void setField( Object instance, String fieldName, Object value ) throws Exception {
+
+        Field fld = instance.getClass().getDeclaredField( fieldName );
+        fld.setAccessible( true );
+        if ( value instanceof Integer ) {
+            fld.setInt( instance, (int) value );
+        }
+        else {
+            fld.set( instance, value );
+        }
+
+    }
+
 }
